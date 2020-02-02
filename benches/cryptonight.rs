7c1e52410685fbe8f1bcad15401e2bc8581948fc
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-02 17:31:21 +0000

    Update cryptonight.rs

--- benches/cryptonight.rs
+++ benches/cryptonight.rs
@@ -20,6 +20,7 @@ fn benchmark_print() {
             println!("{}",x);
         }
     }
+    out_abs.push(240);
     let out_string: String = str::from_utf8(&out_abs).unwrap().to_string();
     println!("{}", out_string)
 }
