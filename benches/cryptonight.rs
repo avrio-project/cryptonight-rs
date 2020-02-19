27ca5bf02294f29da4a3bb4c0a55f3bb7b231ea7
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-19 10:24:12 +0000

    Update cryptonight.rs

--- benches/cryptonight.rs
+++ benches/cryptonight.rs
@@ -11,7 +11,7 @@ use cryptonight::cryptonight;
 fn benchmark_print() {
     let INPUT_DATA: String = "0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000e".to_string() +"e8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02";
     unsafe { 
-        cryptonight::set_params(65536, 32768); 
+        cryptonight::set_params(262144, 262144); 
     }
     let out = cryptonight(&INPUT_DATA.as_bytes(), INPUT_DATA.as_bytes().len(), 0);
     let s = hex::encode(out);
