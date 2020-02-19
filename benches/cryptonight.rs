e22440532cc17e4c7319e6817e15e45a18d2b715
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-19 10:26:40 +0000

    Update cryptonight.rs

--- benches/cryptonight.rs
+++ benches/cryptonight.rs
@@ -9,7 +9,7 @@ use bencher::Bencher;
 use cryptonight::cryptonight;
 
 fn benchmark_print() {
-    let INPUT_DATA: String = "0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000e".to_string() +"e8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02";
+    let INPUT_DATA: String = "0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000e".to_string();
     unsafe { 
         cryptonight::set_params(262144, 262144); 
     }
